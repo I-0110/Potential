7179ba82099f03b3fc56631d524f4bc84ec28766
IvelisBecker <ivelisbecker@gmail.com>
2025-05-05 15:11:25 -0500

    Most recent update

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,6 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+import { useState } from 'react';
+
+interface Candidate {
+    
+}
--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -2,7 +2,92 @@ import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [candidate, setCandidate] = useState(null);
+  const [candidates, setCandidates] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(null);
+  const [searchQuery, setSearchQuery] = useState(' ');
+
+  useEffect(() => {
+    const fetchCandidate = async () => {
+      try {
+        const data = await searchGithubUser();
+        setCandidate(data);
+      } catch (err) {
+        setErr('Failed to fetch candidate');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCandidate();
+  }, []);  
+
+const handleSearch = async () => {
+  setLoading(true);
+  setErr(null);
+  try {
+    const data = await searchGithub(searchQuery);
+    setCandidates(data);
+    setCandidate(data[0]);
+  } catch (err) {
+    setErr('Failed to fetch candidates');
+  } finally {
+    setLoading(false);
+  }
 };
+
+const saveCandidate = () => {
+  if (candidate) {
+    const savedCandidates = JSON.parse(localStorage.getItem('savedCandidates')) || [];
+    savedCandidates.push(candidate);
+    localStorage.setItem('savedCandidates', JSON.stringify(savedCandidates));
+
+    skipCandidate();
+  }
+};
+
+const skipCandidate = async () => {
+  try {
+    const data= await searchGithubUser();
+    setCandidate(data);
+  } catch (err) {
+    setErr('Failed to fetch new candidate');
+  }
+};
+
+if (loading) {
+  return <p>Loading...</p>;
+}
+
+if (err) {
+  return <p>{err}</p>;
+}
+
+return(
+  <div>
+    <h1>CandidateSearch</h1>
+    <input 
+      type='text'
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder='Search for a candidate' />
+      <button onClick={handleSearch}>Search</button>
+    {candidates.length > 0 && ( 
+    <div>
+      {candidates.map((candidate, index) => (
+        <div key={index}>
+          <h2>{candidate.name}</h2>
+          <p>{candidate.username}</p>
+          <p>{candidate.location}</p>
+          <img src={candidate.avatar_url} alt={candidate.name} />
+          <button onClick={saveCandidate}>+</button>
+          <button onClick={skipCandidate}>-</button>
+        </div>
+      ))}
+      </div>
+      )}
+  </div>
+)};
 
 export default CandidateSearch;
